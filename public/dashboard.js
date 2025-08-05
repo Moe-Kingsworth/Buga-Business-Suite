061fe20feb0a81c9f93c35479f7ccd145ae413bc
Moses Kingsworth <Moe-Kingsworth>
2025-08-05 21:39:18 +0100

    updated dashboard

--- public/dashboard.js
+++ public/dashboard.js
@@ -336,4 +336,13 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     }
 
+    
+  function openModal(id) {
+    document.getElementById(id).classList.remove('hidden');
+  }
+
+  function closeModal(id) {
+    document.getElementById(id).classList.add('hidden');
+  }
+
 }); // End DOMContentLoaded
